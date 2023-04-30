03904673487ec203600ecddcc8d3b2b42d1f5c8b
AapoL <47327885+aapolipponen@users.noreply.github.com>
2023-04-30 12:25:05 +0300

    Clearer documentation and some improved code.

--- airfoil_loft.py
+++ airfoil_loft.py
@@ -3,28 +3,22 @@ import fullcontrol as fc
 import math
 import csv
 
-layer_height = 0.3
 
-#These are good settings for my 3d printer. Feel free to change them to your settings. 
-#The speed values are kinda high so many printers probably wont handle it. 
-#Uncomment if you want to 3d print.
 
-#initial_settings = {
-#    "extrusion_width": 0.4,
-#    "extrusion_height": layer_height,
-#    "print_speed": 7000,
-#    "travel_speed": 9000,
-#    "nozzle_temp": 220,
-#    "bed_temp": 55,
-#}
-#gcode_controls = fc.GcodeControls(save_as='my_design', initialization_data=initial_settings)
+def calibration(bed_x_max, bed_y_max):
+    calibration = []
+    calibration.append(fc.Extruder(on=False))
+    calibration.append(fc.Point(x=bed_x_max, y=bed_y_max, z=10))
+    calibration.append(fc.Point(x=0, y=0, z=10))
+    calibration.append(fc.Extruder(on=True))
+    return calibration
 
 def naca_airfoil(naca_num, num_points, z, chord_length):
     """
     Generates the coordinates of a NACA airfoil at a specific z height and chord length.
     
     Parameters:
-        naca_num (str): The four-digit NACA airfoil number.
+        naca_num (str): The NACA airfoil number.
         num_points (int): The number of points to generate.
         z (float): The z-coordinate at which to generate the airfoil.
         chord_length (float): The chord length of the airfoil.
@@ -32,39 +26,43 @@ def naca_airfoil(naca_num, num_points, z, chord_length):
     Returns:
         list: A list of fc.Point objects containing the coordinates of the NACA airfoil at the specified z height and chord length.
     """
-    naca_num = naca_num.zfill(4) # pad the number with leading zeroes if needed
-    
-    m = float(naca_num[0]) / 100  # Maximum camber
-    p = float(naca_num[1]) / 10   # Location of maximum camber
-    t = float(naca_num[2:]) / 100 # Maximum thickness
-    
     x = np.linspace(0, 1, num_points)
-    y_c = np.zeros_like(x)
-    y_t = 5*t * (0.2969*np.sqrt(x) - 0.126*x - 0.3516*x**2 + 0.2843*x**3 - 0.1036*x**4)
     
-    # Calculate camber line and upper/lower surfaces
-    if p == 0 or m == 0:
-        yc = np.zeros_like(x)
-        xu = x
-        xl = x
-        theta = np.zeros_like(x)
+    if len(naca_num) == 4:
+        m = int(naca_num[0]) / 100  # Maximum camber
+        p = int(naca_num[1]) / 10   # Location of maximum camber
+        t = int(naca_num[2:]) / 100 # Maximum thickness
+        
+        y_t = 5*t * (0.2969*np.sqrt(x) - 0.126*x - 0.3516*x**2 + 0.2843*x**3 - 0.1015*x**4)
+        
+        if p == 0 or m == 0:
+            yc = np.zeros_like(x)
+        else:
+            yc = np.where(x < p, m/p**2 * (2*p*x - x**2), m/(1-p)**2 * ((1-2*p) + 2*p*x - x**2))
+        
+    elif len(naca_num) == 5:
+        naca_num = int(naca_num)
+        t = (naca_num % 1000) / 100
+        m = ((naca_num // 1000) % 100) / 100
+        k = (naca_num // 100000) / 100
+        
+        y_t = (t/0.2) * (0.2969*np.sqrt(x) - 0.1260*x - 0.3516*x**2 + 0.2843*x**3 - 0.1036*x**4)
+        yc = (k/0.2) * (0.0580*x**5 - 0.1265*x**4 + 0.3516*x**3 - 0.2843*x**2 + 0.1036*x)
+        
     else:
-        yc = np.where(x < p, m/p**2 * (2*p*x - x**2), m/(1-p)**2 * ((1-2*p) + 2*p*x - x**2))
-        dyc_dx = np.where(x < p, 2*m/p**2 * (p - x), 2*m/(1-p)**2 * (p - x))
-        theta = np.arctan(dyc_dx)
-        xu = x - y_t*np.sin(theta)
-        xl = x + y_t*np.sin(theta)
-    
+        raise ValueError("Invalid NACA number. Must be 4 or 5 digits long.")
+
+    theta = np.arctan(np.gradient(yc, x))
+    xu = x - y_t*np.sin(theta)
+    xl = x + y_t*np.sin(theta)
     yu = yc + y_t*np.cos(theta)
     yl = yc - y_t*np.cos(theta)
     
-    # Scale coordinates based on chord length
     xu *= chord_length
     yu *= chord_length
     xl *= chord_length
     yl *= chord_length
     
-    # Generate fc.Point objects
     steps = []
     for i in range(num_points):
         steps.append(fc.Point(x=xu[i], y=yu[i], z=z))
@@ -74,76 +72,73 @@ def naca_airfoil(naca_num, num_points, z, chord_length):
     return steps
 
 def airfoil_loft(naca_nums, num_points, z_values, chord_lengths, layer_height):
-    """
-    Generates a loft between a series of NACA airfoils at different heights and chord lengths.
-    
-    Parameters:
-        naca_nums (list): A list of the four to six digit NACA airfoil numbers.
-        num_points (int): The number of points to generate for each airfoil.
-        z_values (list): A list of z-values for the airfoils.
-        chord_lengths (list): A list of chord lengths for the airfoils.
-        layer_height (float): The height of the layers in the loft.
-        
-    Returns:
-        list: A list of fc.Point objects containing the coordinates of the loft.
-    """
-    
-    # Generate airfoils at different heights using the given NACA numbers, number of points, z-values, and chord lengths
-    # and store them in a list called airfoils
     airfoils = [naca_airfoil(num, num_points, z, chord) for num, z, chord in zip(naca_nums, z_values, chord_lengths)]
-    
-    
-    # Create layers between adjacent airfoils and store them in a list called steps
+
     steps = []
-    
-    # Iterate over the pairs of adjacent airfoils
+
     for i, (airfoil1, airfoil2) in enumerate(zip(airfoils[:-1], airfoils[1:])):
         
-        # Determine the number of layers between the current and the next airfoil based on the layer height
         num_layers = int((z_values[i+1] - z_values[i]) / layer_height)
-        
-        # Iterate over the layers between the current and the next airfoil
-        for j in range(num_layers):
-            
-            # Determine the interpolation factor (t) for the current layer
-            t = j / num_layers
-            
-            # Create a list of points for the current layer by linearly interpolating between the points of the two
-            # adjacent airfoils based on the interpolation factor t and the z-value of the current layer
-            layer = [fc.Point(x=(1-t)*p1.x + t*p2.x, y=(1-t)*p1.y + t*p2.y, z=z_values[i] + j*layer_height)
-                     for p1, p2 in zip(airfoil1, airfoil2)]
-            
-            # Add the points of the current layer to the list of steps
-            steps.extend(layer)
-    
-    # Return the list of points representing the loft
+
+        if num_layers > 0:
+            for j in range(num_layers):
+                
+                t = j / num_layers
+                
+                layer = [fc.Point(x=p1.x*(1-t) + p2.x*t, y=p1.y*(1-t) + p2.y*t, z=p1.z*(1-t) + p2.z*t)
+                         for p1, p2 in zip(airfoil1, airfoil2)]
+                
+                if i == 0 and j == 0:
+                    steps.extend(layer)
+                else:
+                    steps.append(layer[-1])
+
+                steps.extend(layer[1:] + [layer[0]])
+
     return steps
 
-
-
 # Example usage
+layer_height = 0.3
+line_width = 0.4
+#These are good settings for my 3d printer. Feel free to change them to your settings. 
+#The speed values are kinda high so many printers probably wont handle it.
+settings = {
+    "extrusion_width": line_width,
+    "extrusion_height": layer_height,
+    "print_speed": 6000,
+    "travel_speed": 8000,
+    "nozzle_temp": 220,
+    "bed_temp": 55,
+}
 
-naca_nums = ['4412', '2412']  # List of NACA airfoil numbers
-num_points = 128
-z_values = [0, 25]  # List of z-values for the airfoils
-chord_lengths = [75, 50]  # Chord lengths of the airfoils
+naca_nums = ['2412', '2412']  # List of NACA airfoil numbers
+num_points = 1024  # The resolution / accuracy of your airfoil. 512 = Beatiful, slow 256 = 
+z_values = [0, 40]  # List of z-values for the airfoils
+chord_lengths = [100, 75]  # Chord lengths of the airfoils
 
-#If you want to change the layer height or 3d print go to the top of the script
+#NOTE: If you want to enable 3d printing 
+# uncomment the things down after fc.transform(steps, 'plot', fc.PlotControls(line_width=0.6, color_type='print_sequence'))
 
-design_steps = airfoil_loft(naca_nums, num_points, z_values, chord_lengths, layer_height)
-design_steps.append(fc.Extruder(on=False))
-design_steps.append(fc.Point(x=0, y=0, z=+10))
+steps = airfoil_loft(naca_nums, num_points, z_values, chord_lengths, layer_height)
 
-#Placeholder because of upcoming features.
-steps = design_steps
 
-#Offset the print so it isn't too close to the bed corners.
-#This setting is maybe different on your printer.
+# Offset the generated airfoil.
+# If 3D printing make sure to double check this,
+# because it might be different on different printers and airfoils.
 steps = fc.move(steps, fc.Vector(x=50))
 steps = fc.move(steps, fc.Vector(y=100))
 
+# Show the bed size, with the cost of an extra travel move at the start of the gcode.
+# Works also without 3d printing
+calibration = calibration(bed_x_max = 300, bed_y_max = 300)
+steps = calibration+steps
+     
+# Move extruder up a set amount (Default = 10) after 3D print is done.
+Z_hop = 10
+steps.append(fc.Extruder(on=False))
+steps.append(fc.Point(x=0, y=0, z=+Z_hop))
 
-fc.transform(steps, 'plot', fc.PlotControls(color_type='print_sequence'))
+fc.transform(steps, 'plot', fc.PlotControls(line_width=0.6, color_type='print_sequence'))
 
-#Uncomment this and the settings at the top if you want to 3d print.
-#fc.transform(steps, 'gcode', gcode_controls)
+# Uncomment if you want to 3D print.
+#fc.transform(steps, 'gcode', fc.GcodeControls(save_as='my_design', initialization_data=settings)
