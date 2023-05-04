6df0cabe12bd34493217b720a40dca1f5043a34e
aapolipponen <aapo.lipponen@gmail.com>
2023-05-04 15:43:49 +0300

    Improved the infill

--- infill_test_V2.py
+++ infill_test_V2.py
@@ -69,7 +69,6 @@ def naca_airfoil(naca_num, num_points, z, chord_length):
     
     return steps
 
-
 def airfoil_loft(naca_nums, num_points, z_values, chord_lengths, layer_height):
     airfoils = [naca_airfoil(num, num_points, z, chord) for num, z, chord in zip(naca_nums, z_values, chord_lengths)]
 
@@ -94,8 +93,12 @@ def airfoil_loft(naca_nums, num_points, z_values, chord_lengths, layer_height):
 
                 steps.extend(layer[1:] + [layer[0]])
 
+                # Calculate min_x and max_x using the current layer
+                min_x = min(point.x for point in layer)
+                max_x = max(point.x for point in layer)
+
                 # Apply triangle_wave_infill for the current layer
-                steps = triangle_wave_infill(steps, layer[0].z)
+                steps = triangle_wave_infill(steps, layer[0].z, min_x, max_x)
 
     return steps
 
@@ -120,14 +123,7 @@ def find_closest(point, point_list, ignore, condition):
     return closest_point
 
 
-def triangle_wave_infill(steps, z):
-    # Extract the x values
-    x_values = [point.x for point in steps]
-    
-    # Find the minimum and maximum x values
-    min_x = min(x_values)
-    max_x = max(x_values)
-    
+def triangle_wave_infill(steps, z, min_x, max_x):
     density = 24
     s_density = max_x / density
         
@@ -147,6 +143,7 @@ def triangle_wave_infill(steps, z):
     
     return steps
 
+
 # SETTINGS
 layer_height = 0.3
 line_width = 0.4
@@ -162,7 +159,7 @@ settings = {
 }
 
 naca_nums = ['2412', '2412']  # List of NACA airfoil numbers
-num_points = 256 # The resolution / accuracy of your airfoil. 
+num_points = 128 # The resolution / accuracy of your airfoil. 
 # resolution = graphical quality, generation speed, gcode size (using default settings)
 # 1024 = (Dont use this one) Dimishing returns, so slow you don't want to use this one, 22.3 MB
 # 512 = (Probably don't want to use this either.) Really nice, really slow, 11,2 MB 
@@ -171,31 +168,34 @@ num_points = 256 # The resolution / accuracy of your airfoil.
 # 64 = Worse quality, Fast, 1.3 MB
 # 32 = (Dont use this one) Curves look ok but the leading edge is really really bad, Really fast, 0,7 MB 
 
-z_values = [0, 10]  # List of z-values for the airfoils
-chord_lengths = [100, 100]  # Chord lengths of the airfoils
+z_values = [0, 15]  # List of z-values for the airfoils
+chord_lengths = [100, 75]  # Chord lengths of the airfoils
 
 # NOTE: If you want to enable 3d printing 
-# uncomment the things down after fc.transform(steps, 'plot', fc.PlotControls(line_width=0.6, color_type='print_sequence'))
+# uncomment the things down after fc.transform(steps, 'plot', fc.PlotControls(color_type='print_sequence'))
 
 steps = airfoil_loft(naca_nums, num_points, z_values, chord_lengths, layer_height)
+
+# Debug
+#print(steps)
 
 # Offset the generated airfoil.
 # If 3D printing make sure to double check this,
 # because it might be different on different printers and airfoils.
-#steps = fc.move(steps, fc.Vector(x=50))
-#steps = fc.move(steps, fc.Vector(y=100))
+steps = fc.move(steps, fc.Vector(x=50))
+steps = fc.move(steps, fc.Vector(y=100))
 
 # Show the bed size, with the cost of an extra travel move at the start of the gcode.
 # Works also without 3d printing
-#calibration = calibration(bed_x_max = 300, bed_y_max = 300)
-#steps = calibration+steps
-#    
-## Move extruder up a set amount (Default = 10) after 3D print is done.
-#Z_hop = 10
-#steps.append(fc.Extruder(on=False))
-#steps.append(fc.Point(z=+Z_hop))
+calibration = calibration(bed_x_max = 300, bed_y_max = 300)
+steps = calibration+steps
+    
+## Move extruder up a set amount (Default = 25) after 3D print is done.
+Z_hop = 25
+steps.append(fc.Extruder(on=False))
+steps.append(fc.Point(z=+Z_hop))
 
-fc.transform(steps, 'plot', fc.PlotControls(line_width=0.6, color_type='print_sequence'))
+fc.transform(steps, 'plot', fc.PlotControls(color_type='print_sequence'))
 
 # Uncomment if you want to 3D print / generate GCODE.
 #fc.transform(steps, 'gcode', fc.GcodeControls(save_as='my_design', initialization_data=settings))
